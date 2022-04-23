4bdf337aed15a5ca7060addad4a0a7c725df5d11
Filip <bagiczny3@gmail.com>
2022-04-23 20:14:54 +0200

    Checks if file is able to write or only readable

--- Console_App_review/Console_App_review/Program.cs
+++ Console_App_review/Console_App_review/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(Environment.OSVersion.VersionString);
+﻿using static System.Console;
+
+string path = @"C:\Users\bagic\OneDrive\Pulpit";
+
+
+Write("Press R for read-only or W for writeable: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+
+Stream? s;
+
+if (key.Key == ConsoleKey.R)
+{
+    s = File.Open(
+    Path.Combine(path, "file.txt"),
+    FileMode.OpenOrCreate,
+    FileAccess.Read);
+}
+else
+{
+    s = File.Open(
+    Path.Combine(path, "file.txt"),
+    FileMode.OpenOrCreate,
+    FileAccess.Write);
+}
+
+string message;
+
+switch (s)
+{
+    case FileStream writeableFile when s.CanWrite:
+        message = "The stream is a file that I can write to.";
+        break;
+    case FileStream readOnlyFile:
+        message = "The stream is a read-only file.";
+        break;
+    case MemoryStream ms:
+        message = "The stream is a memory address.";
+        break;
+    default: // always evaluated last despite its current position
+        message = "The stream is some other type.";
+        break;
+    case null:
+        message = "The stream is null.";
+        break;
+}
+WriteLine(message);
