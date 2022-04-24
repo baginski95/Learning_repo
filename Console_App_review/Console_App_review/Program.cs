62a1b27f89ee08222bcca2d4dff7b5ff376fb6e9
Filip <bagiczny3@gmail.com>
2022-04-24 23:27:40 +0200

    Fibbonacci numbers

--- Console_App_review/Console_App_review/Program.cs
+++ Console_App_review/Console_App_review/Program.cs
@@ -86,5 +86,53 @@ static void RunFactorial(long number)
     }
 }
 
-RunFactorial(10);
+static int FibImperative(int term)
+{
+    if (term == 1)
+    {
+        return 0;
+    }
+    else if (term == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return FibImperative(term - 1) + FibImperative(term - 2);
+    }
+}
+
+static string CardinalToOrdinal(int number)
+{
+    switch (number)
+    {
+        case 11: // special cases for 11th to 13th
+        case 12:
+        case 13:
+            return $"{number}th";
+        default:
+            int lastDigit = number % 10;
+            string suffix = lastDigit switch
+            {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+            };
+            return $"{number}{suffix}";
+    }
+}
+
+static void RunFibImperative()
+{
+    for (int i = 1; i <= 30; i++)
+    {
+        WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+        arg0: CardinalToOrdinal(i),
+        arg1: FibImperative(term: i));
+    }
+}
+
+//RunFactorial(10);
+RunFibImperative();
 
