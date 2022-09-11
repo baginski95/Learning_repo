e02495b4ca0ccb57bfc553bf638d0409ba918e06
Filip <bagiczny3@gmail.com>
2022-09-11 17:12:07 +0200

    Coming back to learning

--- OOPChapter/PacktLibrary/Person.cs
+++ OOPChapter/PacktLibrary/Person.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using static System.Console;
+using System.Collections.Generic;
 
 namespace Packt.Shared
 {
-    public class Person
+    public class Person : object
     {
         public string Name;
         public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+        public List<Person> Children = new List<Person>();
     }
+  
 }
--- OOPChapter/PeopleApp/Program.cs
+++ OOPChapter/PeopleApp/Program.cs
@@ -4,7 +4,20 @@ using static System.Console;
 Person bob = new Person();
 
 bob.Name = "Bob Smith";
-bob.DateOfBirth = new DateTime(1965, 12, 22); // C# 1.0 or later
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+bob.BucketList =
+ WondersOfTheAncientWorld.HangingGardensOfBabylon
+ | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
 WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
  arg0: bob.Name,
  arg1: bob.DateOfBirth);
+
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.
+StatueOfZeusAtOlympia;
+WriteLine(
+ format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+ arg0: bob.Name,
+ arg1: bob.FavoriteAncientWonder,
+ arg2: (int)bob.FavoriteAncientWonder);
+
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
