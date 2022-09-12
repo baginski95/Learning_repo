2ffb386d46e4126c3a4c9d80aa6129fd77aaacd4
Filip <bagiczny3@gmail.com>
2022-09-12 22:31:26 +0200

    New, old things

--- OOPChapter/PacktLibrary/Person.cs
+++ OOPChapter/PacktLibrary/Person.cs
@@ -6,11 +6,37 @@ namespace Packt.Shared
 {
     public class Person : object
     {
+        public const string Species = "Homo Sapien";
+        public readonly string HomePlanet = "Earth";
         public string Name;
         public DateTime DateOfBirth;
         public WondersOfTheAncientWorld FavoriteAncientWonder;
         public WondersOfTheAncientWorld BucketList;
         public List<Person> Children = new List<Person>();
+        public readonly DateTime Instantiated;
+        
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        // methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+        public string GetOrigin()
+        {
+            return $"{Name} was born on {HomePlanet}.";
+        }
     }
   
 }
--- OOPChapter/PeopleApp/Program.cs
+++ OOPChapter/PeopleApp/Program.cs
@@ -21,3 +21,53 @@ WriteLine(
  arg2: (int)bob.FavoriteAncientWonder);
 
 WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+
+bob.Children.Add(new Person { Name = "Alfred" }); // C# 3.0 and later
+bob.Children.Add(new() { Name = "Zoe" }); // C# 9.0 and later
+WriteLine(
+ $"{bob.Name} has {bob.Children.Count} children:");
+//for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+//{
+//    WriteLine($" {bob.Children[childIndex].Name}");
+//}
+foreach(var child in bob.Children)
+{
+    WriteLine($" {child.Name}");
+}
+
+BankAccount.InterestRate = 0.012M; // store a shared value
+BankAccount jonesAccount = new()
+{
+    AccountName = "Mrs. Jones",
+    Balance = 2400
+}; // C# 9.0 and later
+WriteLine(format: "{0} earned {1:C} interest.",
+ arg0: jonesAccount.AccountName,
+ arg1: jonesAccount.Balance * BankAccount.InterestRate);
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+WriteLine(format: "{0} earned {1:C} interest.",
+ arg0: gerrierAccount.AccountName,
+ arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+WriteLine($"{bob.Name} is a {Person.Species}");
+
+WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+Person blankPerson = new();
+WriteLine(format:
+ "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+ arg0: blankPerson.Name,
+ arg1: blankPerson.HomePlanet,
+ arg2: blankPerson.Instantiated);
+
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+WriteLine(format:
+ "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+ arg0: gunny.Name,
+ arg1: gunny.HomePlanet,
+ arg2: gunny.Instantiated);
+
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
