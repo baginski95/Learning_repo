b689ab9fce2954a8f435cf149767b83c31cda316
Filip <bagiczny3@gmail.com>
2022-09-15 23:19:52 +0200

    Working with instance and static methods

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person : object
+{
+    // fields
+    public string? Name; // ? allows null
+    public DateTime DateOfBirth;
+    public List<Person> Children = new(); // C# 9 or later
+                                            // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    // static method to "multiply"
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
+    }
+
+    // instance method to "multiply"
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+}
