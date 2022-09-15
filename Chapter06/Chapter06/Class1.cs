19848570663e6371564687a38650041ebdff3322
Filip <bagiczny3@gmail.com>
2022-09-15 22:29:57 +0200

    Removing unnecessary projects

---
+++ Chapter06/Chapter06/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Chapter06
+{
+    public class Class1
+    {
+
+    }
+}
--- NETMVCrepo/NETMVCrepo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NETMVCrepo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
