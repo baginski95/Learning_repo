f49e71aa4884a7ce661da35e7972514d6f4aec96
Filip <bagiczny3@gmail.com>
2022-09-18 23:41:39 +0200

    Just sterted working with generics

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -35,3 +35,25 @@ harry.Poke();
 harry.Poke();
 harry.Poke();
 harry.Poke();
+
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2; // lookup the value that has 2 as its key
+WriteLine(format: "Key {0} has value: {1}",
+ arg0: key,
+ arg1: lookupObject[key]);
+
+WriteLine(format: "Key {0} has value: {1}",
+ arg0: harry,
+ arg1: lookupObject[harry]);
+
+// generic lookup collection
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
