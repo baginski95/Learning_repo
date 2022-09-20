7ebf0d7a535770b1def7ecc12928a298c4463941
Filip <bagiczny3@gmail.com>
2022-09-20 23:03:27 +0200

    Working with interfaces

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -82,3 +82,10 @@ foreach (Person p in people)
 {
     WriteLine($" {p.Name}");
 }
+
+WriteLine("Use PersonComparer's IComparer implementation to sort:");
+Array.Sort(people, new PersonComparer());
+foreach (Person p in people)
+{
+    WriteLine($" {p.Name}");
+}
