091a2ff13d802eccb05d18b09d1d47fe4a2492b0
Filip <bagiczny3@gmail.com>
2022-09-22 18:06:09 +0200

    Inheriting and overriding Classes

--- Chapter06/NullHandling/Program.cs
+++ Chapter06/NullHandling/Program.cs
@@ -1,10 +1,22 @@
-﻿using static System.Console;
+﻿using Packt.Shared;
+using static System.Console;
 
-int thisCannotBeNull = 4;
-//thisCannotBeNull = null; // compile error!
-int? thisCouldBeNull = null;
-WriteLine(thisCouldBeNull);
-WriteLine(thisCouldBeNull.GetValueOrDefault());
-thisCouldBeNull = 7;
-WriteLine(thisCouldBeNull);
-WriteLine(thisCouldBeNull.GetValueOrDefault());
+//int thisCannotBeNull = 4;
+////thisCannotBeNull = null; // compile error!
+//int? thisCouldBeNull = null;
+//WriteLine(thisCouldBeNull);
+//WriteLine(thisCouldBeNull.GetValueOrDefault());
+//thisCouldBeNull = 7;
+//WriteLine(thisCouldBeNull);
+//WriteLine(thisCouldBeNull.GetValueOrDefault());
+Employee john = new()
+{
+    Name = "John Jones",
+    DateOfBirth = new(year: 1990, month: 7, day: 28)
+};
+john.WriteToConsole();
+
+john.EmployeeCode = "JJ001";
+john.HireDate = new(year: 2014, month: 11, day: 23);
+WriteLine($"{john.Name} was hired on {john.HireDate:dd/MM/yy}");
+WriteLine(john.ToString());
